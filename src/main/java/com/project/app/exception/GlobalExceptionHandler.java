cec6ef541c828673b86c8ccd453fc305fe8be0f5
Sonal Tamse <sonal.tamse@gmail.com>
2026-07-18 13:39:57 +0530

    Handle validation errors and return a proper error response with the first validation error message

--- src/main/java/com/project/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/app/exception/GlobalExceptionHandler.java
@@ -14,4 +14,13 @@ public class GlobalExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream().map(
+                error -> error.getField() + ": " +
+                        error.getDefaultMessage()).findFirst().orElse("Validation Failed");
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), errorMessage);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
