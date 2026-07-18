136b43c4f4dc198eb633233e91ef56ee41564226
Sonal Tamse <sonal.tamse@gmail.com>
2026-07-18 13:29:37 +0530

    Handle MovieNotFoundException and return a custom error response

--- src/main/java/com/project/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/app/exception/GlobalExceptionHandler.java
@@ -9,5 +9,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
+    @ExceptionHandler(MovieNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(MovieNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 }
